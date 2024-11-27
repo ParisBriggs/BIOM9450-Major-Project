56481828c133dacc92525d46481d8e60bae686ee
Aayushma-lab <z5310052@ad.unsw.edu.au>
2024-11-27 14:40:11 +1100

    login error page DONE

---
+++ frontend/login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles/styles_login.css">
+</head>
+
+<body>
+<div class="login-container">
+        <h1><img src="images/company_logo.png" class="company_logo"></h1>
+        <h1>NutriMed Login Page</h1>
+        <h3>Login to get started</h3>
+        
+        <!-- Error Message -->
+        <?php
+        if (isset($_GET['error']) && $_GET['error'] == 'invalid_credentials') {
+            echo "<p style='color: red; text-align: center;'>Invalid username or password. Please try again.</p>";
+        }
+        ?>
+         
+
+        <!-- Login Form -->
+        <form action="loginvalidation.php" method="POST">
+            <input type="userName" name="userName" placeholder="Enter Username" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <div class="remember-forgot">
+                <div class="remember">
+                    <input type="checkbox" name="remember">
+                    <label for="remember"> Remember for 30 days</label>
+                </div>
+                <a href="#">Forgot password?</a>
+            </div>
+            <button type="submit">Login</button>
+        </form>
+        <div class="signup">
+            Don't have an account? <a href="#">Sign up</a>
+        </div>
+    </div>
+
+
+</body>
+</html>
